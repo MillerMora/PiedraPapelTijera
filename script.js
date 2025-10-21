8713b2bd9af44b6f2b9e420b0f8525a5dc6a9e0f
Miller Mora <millermora@gmail.com>
2025-10-21 12:22:11 -0500

    Se creo la funcionalidad de comprobar para comparar las elecciones de la maquina y el jugador

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const SELECCION = document.getElementById("lista_t")
     const BOTON = document.getElementById("BOTON")
-    const CONTENEDOR_ELECCION = document.getElementById("eleccion")
+    let CONTENEDOR_ELECCION = document.getElementsByClassName("contenedor")
     let maquina;
+    
+    function comprobacion (seleccion, maquina){
+        const juego = {
+            piedra: "tijera",
+            papel: "piedra",
+            tijera: "papel"
+        };
+
+        if (juego[seleccion] == maquina){
+            return console.log("Maquina saco ", maquina, "\njugador saco", seleccion, "\nGanaste")
+        }
+        if (juego[maquina] == seleccion){
+            return console.log("Maquina saco ", maquina, "\njugador saco", seleccion, "\nGano la maquina")
+        }
+        else{
+            return console.log("Maquina saco ", maquina, "\njugador saco", seleccion, "\nNinguno gano")
+        }
+
+
+
+    }
     BOTON.addEventListener ("click", function(){
-        const random = (Math.random()*3) + 1
+        const random = Math.floor((Math.random()*3) + 1)
+        const seleccionado = SELECCION.value;
 
         if (random == 1){
-            maquina = ""
+            maquina = "piedra"
+            comprobacion(seleccionado,maquina )
         }
-        
-        const seleccionado = SELECCION.value;
-        console.log(seleccion)
+        if (random == 2){
+            maquina = "papel"
+            comprobacion(seleccionado,maquina )
+        }
+        if (random == 3){
+            maquina = "tijera"
+            comprobacion(seleccionado,maquina )
+        }
     });
