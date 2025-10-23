7aef3b29d5b2290681f07f3525428c50ab502ae6
Miller Mora <millermora2022@gmail.com>
2025-10-22 19:22:05 -0500

    Se adaptó la función de comprobación para funcionar tanto con la máquina como con dos jugadores. Además, se añadió el evento del segundo botón para el modo de dos jugadores.

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
-const SELECCION = document.getElementById("lista_t")
-const BOTON = document.getElementById("boton")
-const CONTENEDOR_ELECCION = document.getElementById("conten")
+SELECCION_J1 = document.getElementById("lista_j1")
+SELECCION_J2 = document.getElementById("lista_j2")
+
+const CONTADOR1 = document.getElementById("contadorj1")
+const CONTADOR2 = document.getElementById("contadorj2")
+
+const ELECCIONMAQUINA = document.getElementById("eleccionMaquina")
+const GANADOR = document.getElementById ("ganador")
 let maquina;
-    
-    function comprobacion (seleccion, maquina){
-        const juego = {
-            piedra: "tijera",
-            papel: "piedra",
+let puntaje1 = 0
+let puntaje2 = 0
+let maquinaesta = false
+
+
+
+function comprobacion (seleccion1, seleccion2, maquinaesta){
+    const juego = {
+        piedra: "tijera",
+        papel: "piedra",
             tijera: "papel"
         };
-
-        if (juego[seleccion] == maquina){
-            return CONTENEDOR_ELECCION.innerHTML = `<p>Maquina saco ${maquina} jugador saco ${seleccion} Ganaste</p>`
+        
+        
+        if (maquinaesta){
+            if (juego[seleccion1] == seleccion2){
+                puntaje1 += 1
+                ELECCIONMAQUINA.innerHTML = `<p id="eleccionMaquina">${seleccion2}</p>`
+                CONTADOR1.innerHTML = `<p id="contadorj1">${puntaje1}</p>`
+                return GANADOR.innerHTML =   ` <p id="ganador">Ganaste</p>`
+            }
+            if (juego[seleccion2] == seleccion1){
+                puntaje2 += 1
+                ELECCIONMAQUINA.innerHTML = `<p id="eleccionMaquina">${seleccion2}</p>`
+                CONTADOR2.innerHTML = `<p id="contadorj2">${puntaje2}</p>`
+                return GANADOR.innerHTML =   ` <p id="ganador">Gano la maquina</p>`
+                
+            }
+            else{
+                ELECCIONMAQUINA.innerHTML = `<p id="eleccionMaquina">${seleccion2}</p>`
+                return GANADOR.innerHTML =   ` <p id="ganador">Ninguno gano</p>`
+            }
         }
-        if (juego[maquina] == seleccion){
-            return CONTENEDOR_ELECCION.innerHTML = `<p>Maquina saco ${maquina} jugador saco ${seleccion} Gano la maquina</p>`
+        
+        if (juego[seleccion1] == seleccion2){
+            puntaje1 += 1
+            CONTADOR1.innerHTML = `<p id="contadorj1">${puntaje1}</p>`
+            return GANADOR.innerHTML =   ` <p id="ganador">Gano el jugador 1</p>`
+        }
+        if (juego[seleccion2] == seleccion1){
+            puntaje2 += 1
+            CONTADOR2.innerHTML = `<p id="contadorj2">${puntaje2}</p>`
+            return GANADOR.innerHTML =   ` <p id="ganador">Gano el jugador 2</p>`
+            
         }
         else{
-            return CONTENEDOR_ELECCION.innerHTML = `<p>Maquina saco ${maquina} jugador saco ${seleccion} Ninguno gano</p>`
+            return GANADOR.innerHTML =   ` <p id="ganador">Ninguno gano</p>`
         }
-
-
-
+        
+        
+        
     }
-    BOTON.addEventListener ("click", function(){
-        const random = Math.floor((Math.random()*3) + 1)
-        const seleccionado = SELECCION.value;
-
-        if (random == 1){
-            maquina = "piedra"
-            comprobacion(seleccionado,maquina )
-        }
-        if (random == 2){
-            maquina = "papel"
-            comprobacion(seleccionado,maquina )
-        }
-        if (random == 3){
-            maquina = "tijera"
-            comprobacion(seleccionado,maquina )
-        }
-    });
+    try {
+        const BOTON = document.getElementById("boton")
+        BOTON.addEventListener ("click", function(){
+            const random = Math.floor((Math.random()*3) + 1)
+            const seleccionado = SELECCION_J1.value
+            maquinaesta = true
+            if (random == 1){
+                maquina = "piedra"
+                comprobacion(seleccionado,maquina, maquinaesta )
+            }
+            if (random == 2){
+                maquina = "papel"
+                comprobacion(seleccionado,maquina, maquinaesta )
+                
+            }
+            if (random == 3){
+                maquina = "tijera"
+                comprobacion(seleccionado,maquina, maquinaesta )
+                
+            }
+        });
+    } catch (error) {
+        const BOTON2 = document.getElementById("boton2")
+        BOTON2.addEventListener ("click", function(){
+            const seleccionado = SELECCION_J1.value
+            const seleccionado2 = SELECCION_J2.value
+            comprobacion(seleccionado,seleccionado2, maquinaesta )
+            maquinaesta = false
+        });
+    }
+    
